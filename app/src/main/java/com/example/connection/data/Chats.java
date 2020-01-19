5eccbf92decd8010de0c39095df755b2a5b01a62
jeprose <jadenelkins@gmail.com>
2020-01-19 03:38:59 -0800

    yop

--- app/src/main/java/com/example/connection/data/Chats.java
+++ app/src/main/java/com/example/connection/data/Chats.java
@@ -1,13 +1,14 @@
 package com.example.connection.data;
+import java.io.Serializable;
 import java.util.ArrayList;
-public class Chats {
+public class Chats implements Serializable {
     ArrayList<User> users;
-    ArrayList<Message> messages;
+    public ArrayList<Message> messages;
     public Chats(ArrayList<User> use, ArrayList<Message> messag){
         users=use;
         messages=messag;
     }
     public String toString(){
-        return users.get(1).getDisplayName()+"\n";
+        return users.get(1).getDisplayName()+"\n"+messages.get(messages.size()-1).text;
     }
 }
