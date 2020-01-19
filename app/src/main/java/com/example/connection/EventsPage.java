57b2d41bb5925d578ec1cf0f0dedbfaec48d8188
jeprose <jadenelkins@gmail.com>
2020-01-18 22:45:53 -0800

    yop

--- app/src/main/java/com/example/connection/EventsPage.java
+++ app/src/main/java/com/example/connection/EventsPage.java
@@ -2,6 +2,8 @@ package com.example.connection;
 
 import android.os.Bundle;
 
+import com.example.connection.data.Chats;
+import com.example.connection.data.model.LoggedInUser;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -14,25 +16,37 @@ import android.content.Intent;
 import android.util.Log;
 
 import android.view.View;
-import java.util.ArrayList;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import com.example.connection.data.User;
+import com.example.connection.data.Message;
 public class EventsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView  listView = (ListView)findViewById(R.id.listview);
+        ListView  listView = findViewById(R.id.listview);
 
-        ArrayList<Event> eventList = new ArrayList<>();
-        eventList.add(new Event("Test Event 1", "This is a test event.", "3:00PM 2/1/20"));
-        eventList.add(new Event("Test Event 2", "This is a test event.", "3:00PM 2/1/20"));
-        eventList.add(new Event("Test Event 3", "This is a test event.", "3:00PM 2/1/20"));
+        ArrayList<Chats> chatList = new ArrayList<Chats>();
+        ArrayList<User> users= new ArrayList<User>();
+        LoggedInUser loged= LoggedInUser.loggedin;
+        User user1= new User("Santhoshi Ramanathan");
+        User user2= new User("Ryan Stear");
+        ArrayList<User> set1= new ArrayList<User>();
+        set1.add(loged);
+        set1.add(user1);
+        loged.addChat(new Chats(set1,new ArrayList<Message>()));
+        ArrayList<User> set2=new ArrayList<User>();
+        set2.add(loged);
+        set2.add(user1);
+        loged.addChat(new Chats(set2,new ArrayList<Message>()));
 
-        ArrayAdapter eventAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, eventList);
+        ArrayAdapter eventAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, loged.getChats());
 
         listView.setAdapter(eventAdapter);
     }
