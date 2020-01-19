36808d70179658447fe295d581c5f41a304c2141
jeprose <jadenelkins@gmail.com>
2020-01-18 17:44:54 -0800

    yop

--- app/src/main/java/com/example/connection/Chat.java
+++ app/src/main/java/com/example/connection/Chat.java
@@ -1,44 +1,32 @@
 package com.example.connection;
 
 import android.os.Bundle;
-import com.example.connection.data.MessageAdapter;
-import com.example.connection.data.Message;
 
-import com.example.connection.data.model.LoggedInUser;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.room.Room;
 
-import android.widget.EditText;
 import android.view.View;
-import android.widget.ListView;
-
-import java.lang.reflect.Member;
 
 public class Chat extends AppCompatActivity {
-    private EditText editText;
-    private MessageAdapter messageAdapter;
-    private ListView messagesView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_chat);
-        // This is where we write the mesage
-        editText = (EditText) findViewById(R.id.editText);
-        messageAdapter = new MessageAdapter(this);
-        messagesView = (ListView) findViewById(R.id.messages_view);
-        messagesView.setAdapter(messageAdapter);
-    }
-    public void sendMessage(View view){
-        String message = editText.getText().toString();
-        onMessage(new Message(LoggedInUser.loggedin,message));
-    }
-    public void onMessage(final Message m) {
-        messageAdapter.add(m);
-        // scroll the ListView to the last added element
-        messagesView.setSelection(messagesView.getCount() - 1);
+        setContentView(R.layout.activity_chat);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
