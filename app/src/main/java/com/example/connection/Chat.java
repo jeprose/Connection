e8624fb918df26ecfc9ff17f32bb9027f9f2917f
jeprose <jadenelkins@gmail.com>
2020-01-19 02:20:46 -0800

    yop

--- app/src/main/java/com/example/connection/Chat.java
+++ app/src/main/java/com/example/connection/Chat.java
@@ -1,26 +1,44 @@
 package com.example.connection;
 
 import android.os.Bundle;
+import com.example.connection.data.MessageAdapter;
+import com.example.connection.data.Message;
 
+import com.example.connection.data.model.LoggedInUser;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
+
 import android.widget.EditText;
 import android.view.View;
+import android.widget.ListView;
+
+import java.lang.reflect.Member;
 
 public class Chat extends AppCompatActivity {
     private EditText editText;
+    private MessageAdapter messageAdapter;
+    private ListView messagesView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_chat);
         // This is where we write the mesage
         editText = (EditText) findViewById(R.id.editText);
+        messageAdapter = new MessageAdapter(this);
+        messagesView = (ListView) findViewById(R.id.messages_view);
+        messagesView.setAdapter(messageAdapter);
     }
     public void sendMessage(View view){
         String message = editText.getText().toString();
-
+        onMessage(new Message(LoggedInUser.loggedin,message));
+    }
+    public void onMessage(final Message m) {
+        messageAdapter.add(m);
+        // scroll the ListView to the last added element
+        messagesView.setSelection(messagesView.getCount() - 1);
     }
 }
--- app/src/main/java/com/example/connection/EventsPage.java
+++ app/src/main/java/com/example/connection/EventsPage.java
@@ -1,36 +1,27 @@
 package com.example.connection;
-
 import android.os.Bundle;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
 import com.example.connection.data.Chats;
 import com.example.connection.data.model.LoggedInUser;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.AdapterView;
-import android.content.Intent;
-import android.util.Log;
 
-import android.view.View;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import com.example.connection.data.User;
 import com.example.connection.data.Message;
-public class EventsPage extends AppCompatActivity {
 
+public class EventsPage extends AppCompatActivity{
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_page);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        ListView  listView = findViewById(R.id.listview);
 
         ArrayList<Chats> chatList = new ArrayList<Chats>();
         ArrayList<User> users= new ArrayList<User>();
@@ -45,16 +36,12 @@ public class EventsPage extends AppCompatActivity {
         set2.add(loged);
         set2.add(user2);
         loged.addChat(new Chats(set2,new ArrayList<Message>()));
-
-        ArrayAdapter eventAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, loged.getChats());
-
-        listView.setAdapter(eventAdapter);
     }
 
     /*public void onItemClick(AdapterView<?> l, View v, int position, long id) {
         Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
         Intent intent = new Intent();
-        intent.setClass(this, EventsPage.class);
+        intent.setClass(this, Chat.class);
         intent.putExtra("position", position);
         // Or / And
         intent.putExtra("id", id);
