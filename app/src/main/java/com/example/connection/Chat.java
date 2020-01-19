12b706c89d4b22be429a99b38bae6560f7a675b3
jeprose <jadenelkins@gmail.com>
2020-01-19 02:01:18 -0800

    yop

--- app/src/main/java/com/example/connection/Chat.java
+++ app/src/main/java/com/example/connection/Chat.java
@@ -7,26 +7,20 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.widget.EditText;
 import android.view.View;
 
 public class Chat extends AppCompatActivity {
-
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_chat);
+        // This is where we write the mesage
+        editText = (EditText) findViewById(R.id.editText);
+    }
+    public void sendMessage(View view){
+        String message = editText.getText().toString();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
